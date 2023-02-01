00d564b76fd1553a14a1e9bb817f5883a2c6eed9
Zied Bouazizi <zied.bouazizi@outlook.com>
2023-02-01 17:23:07 +0100

    Added html theme

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Listing;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('listings', [
+        'listings' => Listing::all()
+    ]);
+});
+
+Route::get('/listings/{listing}', function (Listing $listing) {
+    return view('listing', [
+        'listing' => $listing
+    ]);
 });
