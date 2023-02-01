387155d8a941174667991bc17742b9a2539ff432
Zied Bouazizi <zied.bouazizi@outlook.com>
2023-02-01 18:36:20 +0100

    Added card component

--- resources/views/components/list-card.blade.php
+++ resources/views/components/list-card.blade.php
@@ -1,6 +1,6 @@
 @props(['listing'])
 
-<div class="bg-gray-50 border border-gray-200 rounded p-6">
+<x-card>
     <div class="flex">
         <img class="hidden w-48 mr-6 md:block" src="{{ asset('images/laravel-logo.png') }}" alt="" />
         <div>
@@ -27,4 +27,4 @@
             </div>
         </div>
     </div>
-</div>
+</x-card>
--- resources/views/listing.blade.php
+++ resources/views/listing.blade.php
@@ -4,7 +4,7 @@
     <a href="/" class="inline-block text-black ml-4 mb-4"><i class="fa-solid fa-arrow-left"></i> Back
     </a>
     <div class="mx-4">
-        <div class="bg-gray-50 border border-gray-200 p-10 rounded">
+        <x-card class="p-10">
             <div class="flex flex-col items-center justify-center text-center">
                 <img class="w-48 mr-6 mb-6" src="{{ asset('images/laravel-logo.png') }}" alt="" />
 
@@ -47,6 +47,6 @@
                     </div>
                 </div>
             </div>
-        </div>
+        </x-card>
     </div>
 @endsection
