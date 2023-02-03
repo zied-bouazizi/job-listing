ee6d86b445bac94682e698e5b9e64a01f3991ad5
Zied Bouazizi <zied.bouazizi@outlook.com>
2023-02-03 11:24:38 +0100

    Added user & listing relationship

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use App\Models\Listing;
 use Illuminate\Database\Seeder;
 
@@ -15,13 +16,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $user = User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@ecample.com',
+        ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
-
-        Listing::factory(6)->create();
+        Listing::factory(6)->create([
+            'user_id' => $user->id,
+        ]);
     }
 }
